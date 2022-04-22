71ea290736acc8343ac8b7a466fa60e85e62f287
ifeoluwaayo <adeayomide112@gmail.com>
2022-04-22 17:16:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ function App() {
 				<NavBar accounts={accounts} setAccounts={setAccounts} />
 				<MainMint accounts={accounts} setAccounts={setAccounts} />
 			</div>
-			<div className="moving-background"></div>
+			<div className="moving-background">
+				<video loop autoPlay muted>
+					<source
+						src={require("./assets/background/alt.mp4")}
+						type="video/mp4"
+					/>
+				</video>
+			</div>
 		</div>
 	);
 }
